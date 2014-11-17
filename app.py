b9e535c3d0fd51d333d51d72afcf13ee5d82d85d
Eric van Zanten <eric@bahai.us>
2014-11-17 16:58:49 -0600

    Changing default port

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def print_page():
     return resp
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5002))
     app.run(host='0.0.0.0', port=port, debug=True)
