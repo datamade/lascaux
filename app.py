b50c97f584c44abe008c451d995328b86a84d144
Eric van Zanten <eric@bahai.us>
2014-11-17 16:55:45 -0600

    Added an error when missing params

--- app.py
+++ app.py
@@ -15,6 +15,17 @@ app.url_map.strict_slashes = False
 
 @app.route('/', methods=['GET'])
 def print_page():
+    dimensions = request.args.getlist('dimensions')
+    zoom = request.args.get('zoom')
+    center = request.args.get('center')
+    if not dimensions or not zoom or not center:
+        r = {
+            'status': 'error',
+            'message': "'dimensions', 'zoom', and 'center' are required parameters"
+        }
+        resp = make_response(json.dumps(r), 400)
+        resp.headers['content-type'] = 'application/json'
+        return resp
     print_data = {
         'dimensions': request.args['dimensions'].split(','),
         'zoom': request.args['zoom'],
@@ -28,7 +39,6 @@ def print_page():
     tiles_across = math.ceil(float(short_side) / 256.0)
     tiles_up = math.ceil(float(long_side) / 256.0)
     page_size = (int(short_side), int(long_side), int(tiles_across), int(tiles_up),)
-    print print_data
     path = pdfer(print_data, page_size=page_size)
     resp = make_response(open(path, 'rb').read())
     resp.headers['Content-Type'] = 'application/pdf'
